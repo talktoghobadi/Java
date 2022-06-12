496ad54f06bde403d80a84203420446a396e5cdf
Maryam Ghobadi <talktoghobadi@gmail.com>
2022-06-11 21:52:52 -0400

    Add files via upload

---
+++ SongLyrics.java
@@ -0,0 +1,10 @@
+class SongLyrics
+{
+   public static void main(String[] args)
+   {
+      System.out.println("Somewhere over the rainbow");
+      System.out.println("Way up high");
+      System.out.println("There's a land that I heard of");
+      System.out.println("Once in a lullaby");
+   }
+}
